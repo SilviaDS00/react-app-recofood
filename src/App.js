ddc72aa5619170d1213ace9cb0403d77f62ee593
Silvia Donaire <silviadonaireserrano@gmail.com>
2024-02-16 23:32:49 +0100

    feat: add buttons logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 /* App.js */
 
-import React, { useState } from 'react';
-import WebcamComponent from './components/Webcam/WebcamComponent';
-import ImageUploadComponent from './components/ImageUpload/ImageUploadComponent';
-import './App.scss';
+import React from "react";
+import WebcamComponent from "./components/Webcam/WebcamComponent";
+import ImageUploadComponent from "./components/ImageUpload/ImageUploadComponent";
+import "./App.scss";
 
 function App() {
-  const [showWebcam, setShowWebcam] = useState(false);
-
-  const handleButtonClick = () => {
-    setShowWebcam(!showWebcam);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <div className="button-wrapper">
-          <button className="button-styled" onClick={handleButtonClick}>
-            {showWebcam ? 'Cerrar Cámara' : 'Hacer Foto con Cámara'}
-          </button>
-        </div>
-        {showWebcam && <WebcamComponent />}
+        <WebcamComponent />
         <h1>O</h1>
         <ImageUploadComponent />
       </header>
